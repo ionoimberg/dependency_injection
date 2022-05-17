3988e53fa59edbd9b72c32d0683d6a2008fd9335
Ion Oimberg <ionoimberg@gmail.com>
2022-05-17 16:07:35 +0300

    Created Model Main Class, and injected in Block Example.php, called in .phtml

--- DependencyExample/Block/Example.php
+++ DependencyExample/Block/Example.php
@@ -1,23 +1,38 @@
 <?php
+//Block, Layouts, Templates. Injected Class Main from Model. 
+// Model -> Block -> Layout -> templates
 declare(strict_types=1);
 
 namespace Training\DependencyExample\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
+use Training\DependencyExample\Model\Main;
 
-class Example extends Template
+class Example extends Template //used in layuot dependency_index_index.xml
 {
-    
+    /**
+     * @var Main
+     */
+    protected Main $main;
+
     /**
      * @param Context $context
+     * @param Main $main
      * @param array $data
      */
     public function __construct(
         Context $context,
-        array   $data = []
+        Main    $main,      // (required, has no default value. Required always has to be before the optional argument)
+        array   $data = [] //optional, because has a default value
     )
     {
         parent::__construct($context, $data);
+        $this->main = $main;
+    }
+
+    public function getMain(): Main
+    {
+        return $this->main;
     }
 }
---
+++ DependencyExample/Model/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Training\DependencyExample\Model;
+
+class Main
+{
+    public function getId(): string
+    {
+        return 'Class Main';
+    }
+}
