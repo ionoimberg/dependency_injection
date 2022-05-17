5a833bd12acc8f6a99b36209240e947f8d81b07e
Ion Oimberg <ionoimberg@gmail.com>
2022-05-17 16:44:47 +0300

    Get data in Main.php from di.xml, instead of hardcoding

--- DependencyExample/Model/Main.php
+++ DependencyExample/Model/Main.php
@@ -6,8 +6,17 @@ namespace Training\DependencyExample\Model;
 
 class Main
 {
+    /**
+     * @var array
+     */
+    protected array $data;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
     public function getId(): string
     {
-        return 'Class Main';
+        return $this->data['id'];
     }
 }
